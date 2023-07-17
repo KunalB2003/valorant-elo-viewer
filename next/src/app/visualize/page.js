9b2d20aee1da34e9293a433a810edd9cb8a7e8f8
James Liu <liuxjames88@gmail.com>
2023-07-17 14:18:34 -0400

    add visualize page with some static data

--- next/src/app/visualize/page.js
+++ next/src/app/visualize/page.js
@@ -1,6 +1,67 @@
+'use client';
+
+import { Line } from 'react-chartjs-2';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
 
 
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top',
+    },
+    title: {
+      display: true,
+      text: 'Chart.js Line Chart',
+    },
+  },
+};
+
+const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: 'Dataset 1',
+      data: [1, 4, 6, 2, 8, 4, 5],
+      borderColor: 'rgb(255, 99, 132)',
+      backgroundColor: 'rgba(255, 99, 132, 0.5)',
+    },
+    {
+      label: 'Dataset 2',
+      data: [7, 2, 8, 0, 4, 7, 2],
+      borderColor: 'rgb(53, 162, 235)',
+      backgroundColor: 'rgba(53, 162, 235, 0.5)',
+    },
+  ],
+};
 export default function Page() {
-    return <h1>this is the visualize page</h1>;
+
+  // let options = {}
+  // let data = {}
+
+  return <div>
+    <Line options={options} data={data}></Line>
+
+  </div>;
 }
