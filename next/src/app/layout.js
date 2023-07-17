94e384a9eaace4899003d31fcf0292f4a1f0badf
James Liu <liuxjames88@gmail.com>
2023-07-17 14:06:12 -0400

    adding scuffed navbar

--- next/src/app/layout.js
+++ next/src/app/layout.js
@@ -1,3 +1,4 @@
+import Nav from './components/Nav'
 import './globals.css'
 import { Inter } from 'next/font/google'
 
@@ -12,7 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <h1>yeah</h1>
+        <Nav/>
         {children}
         </body>
     </html>
