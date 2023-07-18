8f5f9f2836653b9cb1c8fea736ff0a3c053002a4
James Liu <liuxjames88@gmail.com>
2023-07-18 15:02:04 -0400

    /user/ works now

--- next/src/app/components/Nav.js
+++ next/src/app/components/Nav.js
@@ -4,9 +4,9 @@ import styles from'./Nav.module.css'
 
 
 export default function Nav() {
-    return <div class={styles["navbar"]}>
+    return <div className={styles["navbar"]}>
         <span><Link href="/"><h1>Valorant Elo Viewer</h1></Link></span>
-        <span class={styles["gap"]}></span>
+        <span className={styles["gap"]}></span>
         <span><Link href="user/">View a player</Link></span>
         <span><Link href="visualize/">Visualize</Link></span>
     </div>
--- next/src/app/user/[...slug]/page.js
+++ next/src/app/user/[...slug]/page.js
@@ -3,86 +3,87 @@
 import styles from './page.module.css'
 import { useEffect, useState } from 'react';
 
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
+async function getInfo(region, username, tag, setInfo, setRankedInfo, setRecentRanked) {
+  let userInfo = await getUserInfo(username, tag);
+
+  setInfo(info => ({ ...userInfo.data }))
+  getRankedInfo(region, username, tag).then(data_ => {
+    setRankedInfo(rankedInfo => ({ ...data_.data }))
+  })
+  getRecentRanked(region, username, tag).then(data_ => {
+    setRecentRanked(recentRanked => ([...data_.data.slice(0, 5)]))
+    // console.log(data_.data)
+  })
+}
 async function getUserInfo(username, tag) {
-    return fetch(
-      `https://api.henrikdev.xyz/valorant/v1/account/${username}/${tag}`
-    ).then((res) => res.json());
+  return fetcher(
+    `https://api.henrikdev.xyz/valorant/v1/account/${username}/${tag}`
+  );
 }
 
 async function getRankedInfo(region, username, tag) {
-    return fetch(
-      `https://api.henrikdev.xyz/valorant/v1/mmr/${region}/${username}/${tag}`
-    ).then((res) => res.json());
+  return fetcher(
+    `https://api.henrikdev.xyz/valorant/v1/mmr/${region}/${username}/${tag}`
+  );
 }
 
 async function getRecentRanked(region, username, tag) {
-    return fetch(
-        `https://api.henrikdev.xyz/valorant/v1/mmr-history/${region}/${username}/${tag}`
-    ).then((res) => res.json());
+  return fetcher(
+    `https://api.henrikdev.xyz/valorant/v1/mmr-history/${region}/${username}/${tag}`
+  );
 }
 
 
-export default function Page({params}) {
-    if (params['slug'].length != 3) {
-        return <div className="error">
-            <h2>Error: Incorrect URL format</h2>
-            <p>Correct usage: /data/region/username/tag</p>
-        </div>
-    }
-    const [info, setInfo] = useState({});
-    const [rankedInfo, setRankedInfo] = useState({});
-    const [recentRanked, setRecentRanked] = useState([])
-    const [region, username, tag] = params["slug"];
-    // console.log(region, username, tag)
+export default function Page({ params }) {
+  if (params['slug'].length != 3) {
+    return <div className="error">
+      <h2>Error: Incorrect URL format</h2>
+      <p>Correct usage: /data/region/username/tag</p>
+    </div>
+  }
+  const [info, setInfo] = useState({});
+  const [rankedInfo, setRankedInfo] = useState({});
+  const [recentRanked, setRecentRanked] = useState([])
+  const [region, username, tag] = params["slug"];
+  // console.log(region, username, tag)
 
-    // eventually want to use puuid instead of username and tag
-    useEffect(() => {
-        getUserInfo(username, tag).then(data => {
-            // console.log(data); 
-            // puuid, region, account level, name, tag, card: { small, large, wide}
-            // info = data.data;
-            setInfo( info => ({...info, ...data.data}));
-        }).then( () =>{ 
-            getRankedInfo(region, username, tag).then(data_ => {
-                // console.log(data);
-                setRankedInfo( rankedInfo => ({...rankedInfo, ...data_.data}))
-            })
-            getRecentRanked(region, username, tag).then(data_ => {
-                setRecentRanked(recentRanked => ([...recentRanked, ...data_.data]))
-            })
-        })
-    }, []);
+  // eventually want to use puuid instead of username and tag
+  useEffect(() => {
+    getInfo(region, username, tag, setInfo, setRankedInfo, setRecentRanked)
 
+  }, []);
 
-    return (
+  return (
+    <div>
+      <div className={styles["gap"]}></div>
       <div className={styles["card"]}>
         {JSON.stringify(info) == '{}' || JSON.stringify(rankedInfo) == '{}' ? (
-          <div> loading...</div>
+          <span>
+            <div className={styles["gap"]}></div>
+            <div className={styles["loader"]}></div>
+          </span>
         ) : (
           <div>
             <div className={styles["info"]}>
-              <h1>
-                {username}#{tag}
-              </h1>
+              <h1> {username}#{tag} </h1>
               <span>
-                    <img src={rankedInfo["images"]["small"]} />
-                  
-                  <h3>
-                    {/* {JSON.stringify(rankedInfo, null, 2)} */}
-                    {rankedInfo["currenttierpatched"]} - {rankedInfo["ranking_in_tier"]}RR
-                  </h3>
+                <img src={rankedInfo["images"]["small"]} />
+                <h3>{rankedInfo["currenttierpatched"]} - {rankedInfo["ranking_in_tier"]}RR</h3>
               </span>
-              {/* over here loop over the past 0-5 comps and display them in circles
-            green for wins, red for losses
-            also include the rr gained/lost */}
+              {<div className={styles["recentRanked"]}>
+                {recentRanked.map(match => <div key={match.match_id} className={(match.mmr_change_to_last_game < 0 ? styles["loss"] : styles["win"])}>{match.mmr_change_to_last_game}</div>)}
+              </div>}
             </div>
             {/* {JSON.stringify(info, null, 2)} */}
-            <img className={styles["card_img"]} src={info["card"]["large"]}/>
+            <img className={styles["card_img"]} src={info["card"]["large"]} />
           </div>
         )}
       </div>
-    );
+    </div>
+  );
 
-    // return <div>{JSON.stringify(params, null, 2)}</div>
+  // return <div>{JSON.stringify(params, null, 2)}</div>
 }
 
