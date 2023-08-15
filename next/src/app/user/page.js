63273a66411d4fe7845e0729882738213dfc6cd2
James Liu <liuxjames88@gmail.com>
2023-08-15 15:28:44 -0400

    updaing player list to not use async await

--- next/src/app/user/page.js
+++ next/src/app/user/page.js
@@ -5,27 +5,35 @@ import UserCard from "../components/UserCard";
 
 import Link from "next/link";
 
+import { useState, useEffect } from "react";
+
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
-export default async function Page() {
-    let users = null;
-    users = await fetcher(`${process.env.NEXT_PUBLIC_API_URL}/tracked`);
-    return (
-        <div>
-            <h1>Select a user to view:</h1>
-            {users == null ? (
-                <div>loading...</div>
-            ) : (
-                <ul className={styles["player-cards"]}>
-                    {users.map((user) => (
-                        <li key={user.puuid}>
-                            <Link href={`user/${user.region}/${user.username}/${user.tag}`}>
-                                <UserCard user={user} key={user.puuid} />
-                            </Link>
-                        </li>
-                    ))}
-                </ul>
-            )}
-        </div>
-    );
+export default function Page() {
+  const [playerList, setPlayerList] = useState(null);
+
+  useEffect(() => {
+    fetcher(`${process.env.NEXT_PUBLIC_API_URL}/tracked`).then((data) => {
+      setPlayerList(data);
+    });
+  }, []);
+
+  return (
+    <div>
+      <h1>Select a user to view:</h1>
+      {playerList == null ? (
+        <div>loading...</div>
+      ) : (
+        <ul className={styles["player-cards"]}>
+          {playerList.map((user) => (
+            <li key={user.puuid}>
+              <Link href={`user/${user.region}/${user.username}/${user.tag}`}>
+                <UserCard user={user} key={user.puuid} />
+              </Link>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 }
